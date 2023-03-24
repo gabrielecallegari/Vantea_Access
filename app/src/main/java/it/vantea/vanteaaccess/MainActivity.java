552c62384897524016da962286f89df00af0ee05
Gabriele Callegari <g.callegari@itsrizzoli.it>
2023-03-24 11:12:23 +0100

    add: qr button reader

--- app/src/main/java/it/vantea/vanteaaccess/MainActivity.java
+++ app/src/main/java/it/vantea/vanteaaccess/MainActivity.java
@@ -1,14 +1,22 @@
 package it.vantea.vanteaaccess;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private CardView qr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        qr=findViewById(R.id.card);
+
+        qr.setOnClickListener(l -> {
+            Log.d("TAG", "Clicked");
+        });
     }
 }
