13bac10b0a322ae3353b5bffa6f8e32df4045c09
Gabriele <g.callegari@itsrizzoli.it>
2023-03-27 09:29:37 +0200

    add: numbers on registration

--- app/src/main/java/it/vantea/vanteaaccess/MainActivity.java
+++ app/src/main/java/it/vantea/vanteaaccess/MainActivity.java
@@ -7,16 +7,10 @@ import android.os.Bundle;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-    private CardView qr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        qr=findViewById(R.id.card);
-
-        qr.setOnClickListener(l -> {
-            Log.d("TAG", "Clicked");
-        });
     }
 }
